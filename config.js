82312288badfe133e04cc6d8864b144037d4145b
Mohsinnarejo <mohsinnarejo431@gmail.com>
2024-12-24 15:10:18 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "vUpzQJzK#Bm3pGBjGKtL2AxpdyHODwxI7V5D03clQfVbjutD7cVk",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
